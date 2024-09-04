73a7ac218d9f0b6a69ec0ebca4495c1d4a2403bf
Lena Menson <8000438@mail.ru>
2024-09-04 16:42:06 +0300

    первая итерация проекта

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach(function (element) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__title").textContent = element.name;
+  document.querySelectorAll(".card__delete-button").forEach(function (element) {
+    element.addEventListener("click", function () {
+      this.closest(".places__item").remove();
+    });
+  });
+
+  placesList.append(cardElement);
+});
+
+
+
+
+
+
+
+
